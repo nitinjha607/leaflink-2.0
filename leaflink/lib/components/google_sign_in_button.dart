70e2b500f1b9cb985627c42eeecfe023a1a6bc9c
Riddhi Saraf <143543865+riddhisf@users.noreply.github.com>
2024-01-17 20:12:52 +0530

    Update google_sign_in_button.dart

--- leaflink/lib/components/google_sign_in_button.dart
+++ leaflink/lib/components/google_sign_in_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GoogleSignInButton extends StatelessWidget {
   final void Function()? onPressed;
@@ -27,7 +28,10 @@ class GoogleSignInButton extends StatelessWidget {
           SizedBox(width: 8.0),
           Text(
             text,
-            style: TextStyle(fontSize: 16.0),
+            style: TextStyle(
+                        color: Color.fromRGBO(16, 25, 22, 1),
+                        fontFamily: GoogleFonts.kohSantepheap().fontFamily,
+                        fontSize: MediaQuery.of(context).size.height * 0.02),
           ),
         ],
       ),
