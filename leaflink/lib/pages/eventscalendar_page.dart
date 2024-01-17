1f041c27ca6909f29511cbf0ed37c8dbe9d1c343
Divyanshu Pandey <136846389+007divyanshu@users.noreply.github.com>
2024-01-17 19:38:35 +0530

    Update eventscalendar_page.dart

--- leaflink/lib/pages/eventscalendar_page.dart
+++ leaflink/lib/pages/eventscalendar_page.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
 import 'package:leaflink/pages/connect_page.dart';
 import 'package:leaflink/pages/home_page.dart';
@@ -18,99 +16,112 @@ class CalendarPage extends StatefulWidget {
 
 class _CalendarPageState extends State<CalendarPage> {
   int _selectedIndex = 2; // Set the initial index to 2 for the Calendar page
+  double calendarHeight = 0.8; // Initial calendar height, 80% of the screen
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          toolbarHeight: MediaQuery.of(context).size.height * 0.075,
-          automaticallyImplyLeading: false,
-          title: Text("Events Calendar",
-              style: TextStyle(
-                fontFamily: GoogleFonts.comfortaa().fontFamily,
-                fontSize: MediaQuery.of(context).size.height * 0.04,
-                color: const Color.fromRGBO(16, 25, 22, 1),
-              )),
-          backgroundColor: const Color.fromRGBO(97, 166, 171, 1),
-        ),
-        body: SafeArea(
-          child: Stack(
-            children: [
-              Container(
-                  alignment: Alignment.center,
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(246, 245, 235, 1),
-                  )),
-              Container(
-                alignment: Alignment.center,
-                child: Container(
-                  margin: const EdgeInsets.all(15),
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Color.fromRGBO(204, 221, 221, 1),
-                  ),
-                  child: SfCalendar(
-                    view: CalendarView.month,
-                    // Add any additional properties or controllers as needed
-                  ),
-                ),
-              ),
-            ],
+      appBar: AppBar(
+        toolbarHeight: MediaQuery.of(context).size.height * 0.075,
+        automaticallyImplyLeading: false,
+        title: Text(
+          "Events Calendar",
+          style: TextStyle(
+            fontFamily: GoogleFonts.comfortaa().fontFamily,
+            fontSize: MediaQuery.of(context).size.height * 0.04,
+            color: const Color.fromRGBO(16, 25, 22, 1),
           ),
         ),
+        backgroundColor: const Color.fromRGBO(97, 166, 171, 1),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.61,
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(246, 245, 235, 1),
+            ),
+            child: SfCalendar(
+              view: CalendarView.month,
+              // Add any additional properties or controllers as needed
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * (1 - calendarHeight),
+            padding: const EdgeInsets.all(14.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle the "Add Event" button press
+                    // You can add logic to show a dialog or navigate to another screen
+                    // for adding events.
+                    print('Add Event button pressed');
+                  },
+                  child: Text('Add Event'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle the "Show Events" button press
+                    // You can add logic to show a dialog or navigate to another screen
+                    // for displaying events.
+                    print('Show Events button pressed');
+                  },
+                  child: Text('Show Events'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
 
-        //navbar
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: const Color.fromRGBO(97, 166, 171, 1),
-          type: BottomNavigationBarType.fixed,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_filled),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.connect_without_contact),
-              label: 'Connect',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_month),
-              label: 'Calendar',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.leaderboard),
-              label: 'Leaderboard',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: const Color.fromRGBO(16, 25, 22, 1),
-          unselectedItemColor: const Color.fromRGBO(57, 80, 92, 1),
-          onTap: (index) {
-            setState(() {
-              _selectedIndex = index;
-            });
+      //navbar
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromRGBO(97, 166, 171, 1),
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.connect_without_contact),
+            label: 'Connect',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard),
+            label: 'Leaderboard',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color.fromRGBO(16, 25, 22, 1),
+        unselectedItemColor: const Color.fromRGBO(57, 80, 92, 1),
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
 
-            switch (index) {
-              case 0:
-                // Navigate to the HomePage
-                Navigator.pushNamed(context, HomePage.routeName);
-                break;
-              case 1:
-                // Navigate to the ConnectPage
-                Navigator.pushNamed(context, ConnectPage.routeName);
-                break;
-              case 2:
-                // Navigate to CalendarPage
-                break;
-              case 3:
-                // Navigate to the LeaderboardPage
-                Navigator.pushNamed(context, LeaderboardPage.routeName);
-                break;
-            }
-          },
-        ));
+          switch (index) {
+            case 0:
+              Navigator.pushNamed(context, HomePage.routeName);
+              break;
+            case 1:
+              Navigator.pushNamed(context, ConnectPage.routeName);
+              break;
+            case 2:
+              // Handle navigation or other actions for the Calendar page
+              break;
+            case 3:
+              Navigator.pushNamed(context, LeaderboardPage.routeName);
+              break;
+          }
+        },
+      ),
+    );
   }
 }
