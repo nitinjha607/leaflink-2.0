985f95070a850dd61aab79ca18688c6f71cdeb36
NITIN JHA <115605662+nitinjha607@users.noreply.github.com>
2024-01-17 19:49:10 +0530

    Update my_button.dart

--- leaflink/lib/components/my_button.dart
+++ leaflink/lib/components/my_button.dart
@@ -13,28 +13,22 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap, // Use the onTap callback passed to the widget
-      child: Container(
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
         padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.0135),
-        margin: EdgeInsets.only(
-          left: MediaQuery.of(context).size.width * 0.085,
-          right: MediaQuery.of(context).size.width * 0.085,
-          bottom: MediaQuery.of(context).size.height * 0.009,
-          top: MediaQuery.of(context).size.height * 0.009,
-        ),
-        decoration: BoxDecoration(
-          color: const Color.fromRGBO(97, 166, 171, 1),
+        primary: const Color.fromRGBO(97, 166, 171, 1),
+        shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(6),
         ),
-        child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-              color: const Color.fromRGBO(246, 245, 235, 1),
-              fontSize: 16,
-              fontFamily: GoogleFonts.comfortaa().fontFamily,
-            ),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            color: const Color.fromRGBO(246, 245, 235, 1),
+            fontSize: 16,
+            fontFamily: GoogleFonts.comfortaa().fontFamily,
           ),
         ),
       ),
