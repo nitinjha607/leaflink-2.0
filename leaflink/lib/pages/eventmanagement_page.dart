01fd2e9cb166c394d621d8001d7ccb23968e3a0f
Divyanshu Pandey <136846389+007divyanshu@users.noreply.github.com>
2024-02-10 20:00:52 +0530

    Update eventmanagement_page.dart

--- leaflink/lib/pages/eventmanagement_page.dart
+++ leaflink/lib/pages/eventmanagement_page.dart
@@ -1,6 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:leaflink/components/my_textfield.dart';
+import 'package:leaflink/components/my_button.dart';
 
 class EventManagementPage extends StatefulWidget {
   static const String routeName = 'eventmanagement_page';
@@ -10,45 +13,32 @@ class EventManagementPage extends StatefulWidget {
 }
 
 class _EventManagementPageState extends State<EventManagementPage> {
+  final titleController = TextEditingController();
+  final venueController = TextEditingController();
+  final dateController = TextEditingController();
+  final timeController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   late DateTime _selectedDate;
   late TimeOfDay _selectedTime;
+
+  String _formatDate(DateTime date) {
+    return DateFormat('dd/MM/yyyy').format(date);
+  }
+
+  String _formatTime(TimeOfDay timeOfDay) {
+    final now = DateTime.now();
+    final time = DateTime(
+        now.year, now.month, now.day, timeOfDay.hour, timeOfDay.minute);
+    return DateFormat.jm().format(time);
+  }
 
   @override
   void initState() {
     super.initState();
     _selectedDate = DateTime.now();
     _selectedTime = TimeOfDay.now();
-  }
-
-  Future<void> _addEventToFirestore(
-      String title, String venue, DateTime date, TimeOfDay time) async {
-    try {
-      // Combine date and time into a single DateTime object
-      final DateTime combinedDateTime = DateTime(
-        date.year,
-        date.month,
-        date.day,
-        time.hour,
-        time.minute,
-      );
-
-      // Convert DateTime to Timestamp
-      final Timestamp timestamp = Timestamp.fromDate(combinedDateTime);
-
-      await FirebaseFirestore.instance.collection('events').add({
-        'title': title,
-        'venue': venue,
-        'datetime': timestamp, // Store combined date and time as Timestamp
-      });
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Event added successfully')),
-      );
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error adding event: $e')),
-      );
-    }
+    dateController.text = _formatDate(_selectedDate);
+    timeController.text = _formatTime(_selectedTime);
   }
 
   @override
@@ -58,12 +48,12 @@ class _EventManagementPageState extends State<EventManagementPage> {
         title: Text(
           'Event Management',
           style: TextStyle(
-            fontFamily: 'YourFont', // Change to your desired font family
-            fontWeight: FontWeight.bold,
+            fontFamily: GoogleFonts.comfortaa().fontFamily,
+            fontSize: MediaQuery.of(context).size.height * 0.03,
+            color: const Color.fromRGBO(16, 25, 22, 1),
           ),
         ),
-        backgroundColor: Color.fromRGBO(
-            97, 166, 171, 1), // Match CalendarPage's app bar color
+        backgroundColor: Color.fromRGBO(97, 166, 171, 1),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -72,99 +62,55 @@ class _EventManagementPageState extends State<EventManagementPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Title',
-                ),
-                style: TextStyle(
-                  fontFamily: 'YourFont', // Change to your desired font family
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter title';
-                  }
-                  return null;
-                },
+              MyTextField(
+                controller: titleController,
+                hintText: 'Title',
+                obscureText: false,
               ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Venue',
-                ),
-                style: TextStyle(
-                  fontFamily: 'YourFont', // Change to your desired font family
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter venue';
-                  }
-                  return null;
-                },
+              MyTextField(
+                controller: venueController,
+                hintText: 'Venue',
+                obscureText: false,
               ),
-              SizedBox(height: 20),
               Text(
                 'Select Date:',
                 style: TextStyle(
-                  fontFamily: 'YourFont', // Change to your desired font family
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+                  fontFamily: GoogleFonts.kohSantepheap().fontFamily,
+                  fontSize: MediaQuery.of(context).size.height * 0.02,
+                  color: const Color.fromRGBO(16, 25, 22, 1),
                 ),
               ),
               SizedBox(height: 10),
-              _buildCalendar(),
+              InkWell(
+                onTap: () {
+                  _selectDate(context);
+                },
+                child: IgnorePointer(
+                  child: MyTextField(
+                    controller: dateController,
+                    hintText: 'Date',
+                    obscureText: false,
+                  ),
+                ),
+              ),
               SizedBox(height: 20),
               Text(
                 'Select Time:',
                 style: TextStyle(
-                  fontFamily: 'YourFont', // Change to your desired font family
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+                  fontFamily: GoogleFonts.kohSantepheap().fontFamily,
+                  fontSize: MediaQuery.of(context).size.height * 0.02,
+                  color: const Color.fromRGBO(16, 25, 22, 1),
                 ),
               ),
-              SizedBox(height: 10),
+              SizedBox(height: 20),
               _buildTimePicker(),
               SizedBox(height: 20),
               Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      // Process form data
-                      final title = ''; // Get title from form field
-                      final venue = ''; // Get venue from form field
-
-                      // Parse date string into DateTime object
-                      final List<String> dateParts =
-                          _selectedDate.toString().split('/');
-                      final int year = int.parse(dateParts[2]);
-                      final int month = int.parse(dateParts[0]);
-                      final int day = int.parse(dateParts[1]);
-                      final DateTime selectedDateTime =
-                          DateTime(year, month, day);
-
-                      // Parse time string into TimeOfDay object
-                      final List<String> timeParts =
-                          _selectedTime.toString().split(':');
-                      final int hour = int.parse(timeParts[0]);
-                      final int minute = int.parse(timeParts[1].split(' ')[0]);
-                      final TimeOfDay selectedTimeOfDay =
-                          TimeOfDay(hour: hour, minute: minute);
-
-                      _addEventToFirestore(
-                          title, venue, selectedDateTime, selectedTimeOfDay);
-                    }
+                child: MyButton(
+                  onTap: () {
+                    _submitForm();
                   },
-                  child: Text(
-                    'Add Event',
-                    style: TextStyle(
-                      fontFamily:
-                          'YourFont', // Change to your desired font family
-                    ),
-                  ),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromRGBO(
-                          97, 166, 171, 1), // Match CalendarPage's button color
-                    ),
-                  ),
+                  text: 'Add Event',
                 ),
               ),
             ],
@@ -174,23 +120,74 @@ class _EventManagementPageState extends State<EventManagementPage> {
     );
   }
 
-  Widget _buildCalendar() {
-    return TableCalendar(
-      firstDay: DateTime.utc(2021, 1, 1),
-      lastDay: DateTime.utc(2030, 12, 31),
-      focusedDay: _selectedDate,
-      selectedDayPredicate: (day) {
-        return isSameDay(_selectedDate, day);
-      },
-      onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          _selectedDate = selectedDay;
-        });
-      },
-      calendarStyle: CalendarStyle(
-          // Customize calendar style here
-          ),
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
     );
+    if (picked != null && picked != _selectedDate)
+      setState(() {
+        _selectedDate = picked;
+        dateController.text = _formatDate(_selectedDate);
+      });
+  }
+
+  void _submitForm() {
+    if (_validateForm() && _isDateAfterCurrentDate()) {
+      _addEventToDatabase();
+    } else {
+      String errorMessage = '';
+      if (!_validateForm()) {
+        errorMessage = 'Please fill all the fields.';
+      } else {
+        errorMessage = 'Please select a date after the current date.';
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(errorMessage)),
+      );
+    }
+  }
+
+  bool _validateForm() {
+    return titleController.text.isNotEmpty && venueController.text.isNotEmpty;
+  }
+
+  bool _isDateAfterCurrentDate() {
+    final selectedDateTime = DateTime(
+      _selectedDate.year,
+      _selectedDate.month,
+      _selectedDate.day,
+      _selectedTime.hour,
+      _selectedTime.minute,
+    );
+    final currentDate = DateTime.now();
+    return selectedDateTime.isAfter(currentDate);
+  }
+
+  void _addEventToDatabase() {
+    CollectionReference collRef =
+        FirebaseFirestore.instance.collection('events');
+    collRef
+        .add({
+          'title': titleController.text,
+          'venue': venueController.text,
+          'date': _formatDate(_selectedDate),
+          'time': _formatTime(_selectedTime),
+        })
+        .then((value) => {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Event added successfully')),
+              ),
+              titleController.clear(),
+              venueController.clear(),
+            })
+        .catchError((error) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Failed to add event: $error')),
+          );
+        });
   }
 
   Widget _buildTimePicker() {
@@ -204,6 +201,7 @@ class _EventManagementPageState extends State<EventManagementPage> {
         if (picked != null) {
           setState(() {
             _selectedTime = picked;
+            timeController.text = _formatTime(_selectedTime);
           });
         }
       },
@@ -216,7 +214,7 @@ class _EventManagementPageState extends State<EventManagementPage> {
         child: Text(
           '${_selectedTime.format(context)}',
           style: TextStyle(
-            fontFamily: 'YourFont', // Change to your desired font family
+            fontFamily: GoogleFonts.kohSantepheap().fontFamily,
           ),
         ),
       ),
