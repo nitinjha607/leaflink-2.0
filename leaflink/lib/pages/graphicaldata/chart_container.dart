8c2066cc29a6e0949661a727925bd41a1bac53f1
Divyanshu Pandey <136846389+007divyanshu@users.noreply.github.com>
2024-02-10 19:54:15 +0530

    Update chart_container.dart

--- leaflink/lib/pages/graphicaldata/chart_container.dart
+++ leaflink/lib/pages/graphicaldata/chart_container.dart
@@ -9,30 +9,28 @@ class ChartContainer extends StatelessWidget {
   final String currentMonthValue;
 
   const ChartContainer({
-    super.key,
+    Key? key, // Corrected the super key to key
     required this.title,
     required this.color,
     required this.chart,
     required this.currentMonthValue,
-  });
+  }) : super(key: key); // Added super constructor
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width * 0.95,
-      height: MediaQuery.of(context).size.width * 0.95 * 0.65,
-      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+      height: MediaQuery.of(context).size.width * 0.95,
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(20),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding:
-                const EdgeInsets.only(bottom: 40), // Adjust the value as needed
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 10,
+            left: 20,
             child: Text(
               currentMonthValue,
               style: TextStyle(
@@ -43,41 +41,30 @@ class ChartContainer extends StatelessWidget {
               ),
             ),
           ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.65,
-                  height: MediaQuery.of(context).size.height * 0.15,
-                  child: chart,
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.15,
+            left: 20,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.65,
+              height: MediaQuery.of(context).size.height * 0.15,
+              child: chart,
+            ),
+          ),
+          Positioned(
+            bottom: MediaQuery.of(context).size.height * 0.05,
+            right: 0,
+            child: RotatedBox(
+              quarterTurns: -1,
+              child: Text(
+                'CONTRIBUTIONS',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: MediaQuery.of(context).size.height * 0.022,
+                  color: const Color.fromRGBO(16, 25, 22, 1),
+                  fontFamily: GoogleFonts.comfortaa().fontFamily,
                 ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  RotatedBox(
-                    quarterTurns: -1,
-                    child: Expanded(
-                      child: Column(
-                        children: [
-                          Text(
-                            'CONTRIBUTIONS',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.022,
-                              color: const Color.fromRGBO(16, 25, 22, 1),
-                              fontFamily: GoogleFonts.comfortaa().fontFamily,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+            ),
           ),
         ],
       ),
