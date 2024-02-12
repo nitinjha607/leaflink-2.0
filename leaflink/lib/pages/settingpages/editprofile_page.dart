63b8025fdc11a605fa5a64a9ac4db506c36a37bd
saurabh <kumarsaurabh5009@gmail.com>
2024-02-12 18:23:03 +0530

    view profile is also done hope it will work great

--- leaflink/lib/pages/settingpages/editprofile_page.dart
+++ leaflink/lib/pages/settingpages/editprofile_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as FirebaseAuth;
+import 'package:flutter_share/flutter_share.dart';
+import 'package:leaflink/pages/Create_Post_Page.dart'; // Import the CreatePostPage
 
 class EditProfilePage extends StatelessWidget {
   static const String routeName = 'edit_profile_page';
 
-  const EditProfilePage({super.key});
+  const EditProfilePage({Key? key});
 
   void back(BuildContext context) {
     Navigator.of(context).pop();
@@ -29,6 +33,16 @@ class EditProfilePage extends StatelessWidget {
           onPressed: () => back(context),
           color: const Color.fromRGBO(16, 25, 22, 1),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Redirect to create post page
+              Navigator.pushNamed(context, CreatePostPage.routeName);
+            },
+            icon: Icon(Icons.add),
+            color: Color.fromARGB(255, 0, 0, 0),
+          ),
+        ],
       ),
       body: SafeArea(
         child: Stack(
@@ -41,21 +55,206 @@ class EditProfilePage extends StatelessWidget {
                 color: Color.fromRGBO(246, 245, 235, 1),
               ),
             ),
-            Container(
-              alignment: Alignment.center,
-              child: Container(
-                margin: const EdgeInsets.all(15),
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  color: Color.fromRGBO(204, 221, 221, 1),
-                ),
-              ),
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('posts')
+                  .where('email',
+                      isEqualTo: FirebaseAuth.FirebaseAuth.instance.currentUser!
+                          .email) // Filter posts by user's email
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                } else {
+                  if (snapshot.data!.docs.isEmpty) {
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "No posts found",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Redirect to create post page
+                              Navigator.pushNamed(
+                                  context, CreatePostPage.routeName);
+                            },
+                            child: Text("Create your first post"),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                  return Column(
+                    children: [
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                        child: Text(
+                          FirebaseAuth
+                              .FirebaseAuth.instance.currentUser!.email!,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: RefreshIndicator(
+                          onRefresh: () async {
+                            // Reload posts
+                          },
+                          child: ListView(
+                            children: snapshot.data!.docs
+                                .map((DocumentSnapshot document) {
+                              Map<String, dynamic> data =
+                                  document.data() as Map<String, dynamic>;
+                              bool isLiked = (data['likedBy'] as List<dynamic>)
+                                  .contains(FirebaseAuth.FirebaseAuth.instance
+                                      .currentUser!.email);
+                              return Column(
+                                children: [
+                                  // Card with image, caption, like, share, and delete options
+                                  Card(
+                                    elevation: 5,
+                                    margin: EdgeInsets.all(10),
+                                    child: Column(
+                                      children: [
+                                        ListTile(
+                                          title: Text(
+                                            data['caption'],
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          subtitle: Image.network(
+                                            data['imageUrl'],
+                                            width: double.infinity,
+                                            height: 200,
+                                            fit: BoxFit.cover,
+                                            errorBuilder:
+                                                (context, error, stackTrace) {
+                                              return Icon(Icons.error);
+                                            },
+                                          ),
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            IconButton(
+                                              onPressed: () {
+                                                _likePost(document, isLiked);
+                                              },
+                                              icon: Icon(Icons.favorite),
+                                              color:
+                                                  isLiked ? Colors.red : null,
+                                            ),
+                                            IconButton(
+                                              onPressed: () {
+                                                _sharePost(data['caption'],
+                                                    data['imageUrl']);
+                                              },
+                                              icon: Icon(Icons.share),
+                                              color: Colors.blue,
+                                            ),
+                                            IconButton(
+                                              onPressed: () {
+                                                _showDeleteDialog(
+                                                    context, document);
+                                              },
+                                              icon: Icon(
+                                                Icons.delete,
+                                                color: Colors.red,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                }
+              },
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _showDeleteDialog(BuildContext context, DocumentSnapshot document) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Delete Post"),
+          content: Text("Are you sure you want to delete this post?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                _deletePost(document);
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                "Delete",
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deletePost(DocumentSnapshot document) {
+    FirebaseFirestore.instance.collection('posts').doc(document.id).delete();
+  }
+
+  void _likePost(DocumentSnapshot document, bool isLiked) {
+    if (isLiked) {
+      // Unlike post
+      FirebaseFirestore.instance.collection('posts').doc(document.id).update({
+        'likes': FieldValue.increment(-1),
+        'likedBy': FieldValue.arrayRemove(
+            [FirebaseAuth.FirebaseAuth.instance.currentUser!.email]),
+      });
+    } else {
+      // Like post
+      FirebaseFirestore.instance.collection('posts').doc(document.id).update({
+        'likes': FieldValue.increment(1),
+        'likedBy': FieldValue.arrayUnion(
+            [FirebaseAuth.FirebaseAuth.instance.currentUser!.email]),
+      });
+    }
+  }
+
+  void _sharePost(String caption, String imageUrl) async {
+    await FlutterShare.share(
+      title: 'Check out this post',
+      text: '$caption\n$imageUrl',
     );
   }
 }
