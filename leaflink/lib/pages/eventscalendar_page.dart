c6f0f1a7f29659c1c608ca642b34188abbe64f9e
Riddhi Saraf <143543865+riddhisf@users.noreply.github.com>
2024-02-16 12:43:00 +0530

    Update eventscalendar_page.dart

--- leaflink/lib/pages/eventscalendar_page.dart
+++ leaflink/lib/pages/eventscalendar_page.dart
@@ -6,6 +6,9 @@ import 'package:leaflink/pages/home_page.dart';
 import 'package:leaflink/pages/leaderboard_page.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart'; // Import the necessary package
 import 'package:google_fonts/google_fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/intl.dart';
 
 class CalendarPage extends StatefulWidget {
   static const String routeName = 'calendar_page';
@@ -36,32 +39,60 @@ class _CalendarPageState extends State<CalendarPage> {
         backgroundColor: const Color.fromRGBO(97, 166, 171, 1),
       ),
       body: SafeArea(
-          child: Stack(children: [
-        Container(
-          alignment: Alignment.center,
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: const BoxDecoration(
-            color: Color.fromRGBO(246, 245, 235, 1),
-          ),
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(246, 245, 235, 1),
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height,
+              margin: const EdgeInsets.all(15),
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Color.fromRGBO(204, 221, 221, 1),
+              ),
+              child: StreamBuilder<List<String>>(
+                stream: getDatesStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else {
+                    final List<String> dates = snapshot.data!;
+                    return SfCalendar(
+                      view: CalendarView.month,
+                      todayHighlightColor:
+                          const Color.fromRGBO(97, 166, 171, 1),
+                      dataSource: _getCalendarDataSource(dates),
+                      monthViewSettings: MonthViewSettings(
+                        appointmentDisplayMode:
+                            MonthAppointmentDisplayMode.appointment,
+                      ),
+                      selectionDecoration: BoxDecoration(
+                        color: Colors.transparent,
+                        border: Border.all(
+                            color: const Color.fromRGBO(97, 166, 171, 1),
+                            width: 1),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(4)),
+                        shape: BoxShape.rectangle,
+                      ),
+                      cellEndPadding: 0,
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
         ),
-        Container(
-          height: MediaQuery.of(context).size.height,
-          margin: const EdgeInsets.all(15),
-          width: MediaQuery.of(context).size.width,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            color: Color.fromRGBO(204, 221, 221, 1),
-          ),
-          child: SfCalendar(
-            view: CalendarView.month,
-            todayHighlightColor: const Color.fromRGBO(97, 166, 171, 1),
-
-            // Add any additional properties or controllers as needed
-          ),
-        ),
-      ])),
-
+      ),
       //navbar
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color.fromRGBO(97, 166, 171, 1),
@@ -136,4 +167,42 @@ class _CalendarPageState extends State<CalendarPage> {
       ),
     );
   }
+
+  _AppointmentDataSource _getCalendarDataSource(List<String> dates) {
+    List<Appointment> appointments = [];
+    final DateFormat dateFormat = DateFormat('MM/dd/yyyy');
+    for (String date in dates) {
+      try {
+        DateTime dateTime = dateFormat.parse(date);
+        appointments.add(Appointment(
+          startTime: dateTime,
+          endTime: dateTime.add(Duration(minutes: 10)),
+          subject: 'Event',
+          color: const Color.fromRGBO(97, 166, 171, 1),
+          startTimeZone: '',
+          endTimeZone: '',
+          isAllDay: true,
+        ));
+      } catch (e) {
+        print('Error parsing date: $date');
+        print(e);
+      }
+    }
+    return _AppointmentDataSource(appointments);
+  }
+}
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> dates) {
+    appointments = dates;
+  }
+}
+
+Stream<List<String>> getDatesStream() {
+  return FirebaseFirestore.instance
+      .collection("events")
+      .snapshots()
+      .map((snapshot) {
+    return snapshot.docs.map((doc) => doc['date'] as String).toList();
+  });
 }
