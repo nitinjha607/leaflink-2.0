85f829439e1e26d06edc0b79ad0c7fc03e01c18a
Divyanshu Pandey <136846389+007divyanshu@users.noreply.github.com>
2024-02-18 15:00:59 +0530

    Update added_events.dart

--- leaflink/lib/pages/added_events.dart
+++ leaflink/lib/pages/added_events.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class AddedEventsPage extends StatefulWidget {
   static const String routeName = 'added_events';
@@ -65,7 +66,14 @@ Widget eventsList() {
     future: Firebase.initializeApp(),
     builder: (context, snapshot) {
       if (snapshot.connectionState == ConnectionState.waiting) {
-        return Center(child: CircularProgressIndicator());
+        return Center(
+            child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: LoadingAnimationWidget.dotsTriangle(
+            color: Color.fromRGBO(97, 166, 171, 1),
+            size: 50, // Adjust loader size
+          ),
+        ));
       }
       if (snapshot.hasError) {
         return Center(child: Text('Error: ${snapshot.error}'));
@@ -74,7 +82,14 @@ Widget eventsList() {
         stream: FirebaseFirestore.instance.collection("events").snapshots(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return Center(
+                child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: LoadingAnimationWidget.dotsTriangle(
+                color: Color.fromRGBO(97, 166, 171, 1),
+                size: 50, // Adjust loader size
+              ),
+            ));
           }
           if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
