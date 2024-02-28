361fb60cc26a8dedf39427cfdad1d1faea8c53ee
Divyanshu Pandey <136846389+007divyanshu@users.noreply.github.com>
2024-02-28 17:15:13 +0530

    Update register_page.dart

--- leaflink/lib/pages/register_page.dart
+++ leaflink/lib/pages/register_page.dart
@@ -1,101 +1,198 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:leaflink/auth/login_or_register.dart';
+import 'package:leaflink/components/my_textfield.dart';
 import 'package:leaflink/components/my_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:leaflink/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class WelcomePage extends StatefulWidget {
-  static const String routeName = 'welcome_page';
+class RegisterPage extends StatelessWidget {
+  RegisterPage({Key? key, required this.onTap});
 
-  const WelcomePage({Key? key}) : super(key: key);
+  // text editing controllers
+  final emailController = TextEditingController();
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasController = TextEditingController();
+  final void Function()? onTap;
 
-  @override
-  _WelcomePageState createState() => _WelcomePageState();
-}
+  // Firebase authentication instance
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-class _WelcomePageState extends State<WelcomePage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+  // Register user method
+  Future<void> registerUser(BuildContext context) async {
+    try {
+      // Validate email, username, and passwords
+      if (emailController.text.isEmpty ||
+          usernameController.text.isEmpty ||
+          passwordController.text.isEmpty ||
+          confirmPasController.text.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Please fill in all the fields.'),
+          ),
+        );
+        return;
+      }
 
-  @override
-  void initState() {
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 5), // Adjust the duration as needed
-    )..repeat();
-    super.initState();
-  }
+      if (passwordController.text != confirmPasController.text) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Passwords do not match.'),
+          ),
+        );
+        return;
+      }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+      // Create a user with email and password
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+
+      // Display success message
+      print('User registered successfully!');
+
+      // Navigate to the home page after registration
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      );
+    } catch (e) {
+      // Handle registration errors
+      print('Error during registration: $e');
+      // Display an error message to the user if needed
+      String errorMessage = 'Error during registration. Please try again.';
+      if (e is FirebaseAuthException) {
+        if (e.code == 'email-already-in-use') {
+          errorMessage =
+              'Email is already in use. Please use a different email.';
+        } else {
+          errorMessage = e.message ?? 'An error occurred.';
+        }
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(errorMessage),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment(
-                    0.8,
-                    0.0,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                color: const Color.fromRGBO(97, 166, 171, 1),
+              ),
+              SingleChildScrollView(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    color: const Color.fromRGBO(246, 245, 235, 1),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.08),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
                   ),
-                  colors: [
-                    const Color.fromRGBO(222, 255, 229, 1),
-                    const Color.fromRGBO(190, 213, 235, 1),
-                  ],
+                  height: MediaQuery.of(context).size.height * 0.95,
+                  width: MediaQuery.of(context).size.width * 0.95,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 60),
+                        child: Image.asset(
+                          'assets/images/logo.png',
+                          height: 55.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50.0, bottom: 10),
+                        child: Text(
+                          'Welcome to Leaflink!',
+                          style: TextStyle(
+                            color: const Color.fromRGBO(16, 25, 22, 1),
+                            fontSize:
+                                MediaQuery.of(context).size.height * 0.025,
+                            fontFamily: GoogleFonts.comfortaa().fontFamily,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      MyTextField(
+                        controller: emailController,
+                        hintText: 'Email',
+                        obscureText: false,
+                      ),
+                      MyTextField(
+                        controller: usernameController,
+                        hintText: 'Username',
+                        obscureText: false,
+                      ),
+                      MyTextField(
+                        controller: passwordController,
+                        hintText: 'Password',
+                        obscureText: true,
+                      ),
+                      MyTextField(
+                        controller: confirmPasController,
+                        hintText: 'Re-enter Password',
+                        obscureText: true,
+                      ),
+                      MyButton(
+                        onTap: () => registerUser(context),
+                        text: 'Register',
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Already a member? ",
+                            style: TextStyle(
+                              fontFamily:
+                                  GoogleFonts.kohSantepheap().fontFamily,
+                              fontSize:
+                                  MediaQuery.of(context).size.height * 0.02,
+                              color: Color.fromRGBO(16, 25, 22, 1),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: onTap,
+                            child: Text(
+                              'Login here',
+                              style: TextStyle(
+                                  color: const Color.fromRGBO(97, 166, 171, 1),
+                                  fontSize:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily:
+                                      GoogleFonts.kohSantepheap().fontFamily),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 200.0),
-                    child: Text(
-                      'Welcome to LEAFLINK',
-                      style: TextStyle(
-                        fontSize: MediaQuery.of(context).size.width * 0.08,
-                        fontFamily: GoogleFonts.comfortaa().fontFamily,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(40.0),
-                    child: RotationTransition(
-                      turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
-                      child: Image.asset(
-                        'assets/images/logo.png',
-                        height: 100,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'Your Green Gateway',
-                    style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width * 0.06,
-                      fontFamily: GoogleFonts.comfortaa().fontFamily,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  MyButton(
-                    onTap: () => Navigator.pushNamed(
-                      context,
-                      LogInOrRegister.routeName,
-                    ),
-                    text: 'Continue  →',
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
