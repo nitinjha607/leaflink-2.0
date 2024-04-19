d9a856d5efaeaa821fc6e40bdd55f713ee344f6c
Riddhisf <riddhisaraf8@gmail.com>
2024-04-19 21:33:31 +0530

    fixed svg error and changes in gemini prompt

--- lib/solutionpage.dart
+++ lib/solutionpage.dart
@@ -1,17 +1,32 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:image/image.dart' as img;
 import 'package:google_gemini/google_gemini.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'package:flutter/gestures.dart'; // Import gesture_detector.dart
+import 'package:flutter/gestures.dart';
 
-class SolutionPage extends StatelessWidget {
+class SolutionPage extends StatefulWidget {
   static const String routeName = 'solutionpage';
 
   const SolutionPage({Key? key}) : super(key: key);
+
+  @override
+  State<SolutionPage> createState() => _SolutionPageState();
+}
+
+class _SolutionPageState extends State<SolutionPage> {
+  LatLng? coordinates;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +71,7 @@ class SolutionPage extends StatelessWidget {
                       color: Color.fromRGBO(204, 221, 221, 1),
                     ),
                     child: FutureBuilder<String>(
-                      future: generateContent(imageUrl),
+                      future: generateContent(imageUrl, coordinates),
                       builder: (context, snapshot) {
                         if (snapshot.connectionState ==
                             ConnectionState.waiting) {
@@ -138,17 +153,39 @@ class SolutionPage extends StatelessWidget {
     );
   }
 
-  Future<String> generateContent(String imageUrl) async {
-    final gemini = GoogleGemini(
-        apiKey:
-            "AIzaSyBOF5UUEHg4Io02E5p6QkhIPtccXexa5eQ"); // Replace "YOUR_API_KEY" with your actual API key
+  Future<String> generateContent(String imageUrl, LatLng? coordinates) async {
+    final gemini =
+        GoogleGemini(apiKey: "AIzaSyBOF5UUEHg4Io02E5p6QkhIPtccXexa5eQ");
     final resizedImage = await resizeImage(imageUrl);
     final generatedContent = await gemini.generateFromTextAndImages(
       query:
-          'Identify the waste and categorize in a category of waste and give me three to four pointers under the headings recycle reuse and reduce(be more specific about the recycle heading). In case it is a living object just generate that content cannot be generated for this object',
+          'Identify the waste and categorize in a category of waste and give me three to four pointers under the headings recycle reuse and reduce(be more specific about the recycle heading). In case it is a living object just generate that content cannot be generated for this object and also provide address of nearby recycling centers to the user $coordinates',
       image: resizedImage,
     );
     return generatedContent.text ?? 'No content generated';
+  }
+
+  void _getCurrentLocation() async {
+    LocationData? locationData;
+    try {
+      locationData = await Location().getLocation();
+    } catch (e) {
+      print('Error getting location: $e');
+      // Handle error - display a message to the user or retry obtaining location
+      return;
+    }
+
+    if (locationData != null) {
+      LatLng selectedCoordinates =
+          LatLng(locationData.latitude!, locationData.longitude!);
+      coordinates = selectedCoordinates;
+      print(
+          'Current Location: ${locationData.latitude}, ${locationData.longitude}');
+      setState(() {});
+    } else {
+      // Location data is null, handle this case
+      print('Failed to get current location');
+    }
   }
 
   Future<File> resizeImage(String imageUrl) async {
